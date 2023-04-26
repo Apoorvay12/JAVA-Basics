d199a01d99262411f0a1d08eb1970e20854238a3
Apoorvay12 <122298107+Apoorvay12@users.noreply.github.com>
2023-04-26 18:59:22 +0530

    Add files via upload

---
+++ TernaryOperator.java
@@ -0,0 +1,34 @@
+//Usage of ternary operator
+//Normal code
+
+import java.util.Scanner;
+ class Testj{
+	 public static void main(String ar[]){
+		 int num1,num2;
+		 num1=10;
+		 num2=20;
+		 String res;
+		  if (num1>num2){
+			  System.out.println("Num1 is bigger");
+		        }else{
+			  System.out.println("Num2 is bigger");
+				}
+	 }	  
+ }
+
+
+
+
+//Now Using Ternary operator
+/*
+import java.util.Scanner;
+ class Testj{
+	 public static void main(String ar[]){
+		 int num1,num2;
+		 num1=10;
+		 num2=20;
+		 
+	System.out.println((num1>num2)?"Num1 is bigger":"Num2 is bigger");
+	 }
+ }
+*/
