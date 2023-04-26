a90a7ade7e21aebc4868d5c1eb4d811074e16e89
Apoorvay12 <122298107+Apoorvay12@users.noreply.github.com>
2023-04-26 18:54:07 +0530

    Add files via upload

---
+++ Testj.java
@@ -0,0 +1,499 @@
+/*import java.util.Scanner;
+class Testj{
+
+public static void main(String ar[]){
+double radius,area;
+Scanner scan=new
+Scanner(System.in);
+
+System.out.println("Enter radius");
+radius=scan.nextDouble();
+area=3.14*radius*radius;
+System.out.println("Area of circle is "+area);
+}
+}*/
+
+
+
+/*
+import java.util.Scanner;
+class Testj{
+public static void main(String ar[]){
+double F,convert;
+Scanner scn=new Scanner(System.in);
+System.out.println("Enter the temp in fahrenheit");
+convert=(F-32)*5/9;
+System.out.println("Farenheit to celcius is"+convert);   
+}
+}
+*/
+
+
+/*
+import java.util.Scanner;
+class Testj{
+public static void main(String ar[]){
+Scanner scan=new Scanner(System.in);
+System.out.println("Enter the amount");
+double P=scan.nextDouble();
+System.out.println("Enter the rate of interest");
+double R=scan.nextDouble();
+System.out.println("Enter the time period");
+double T=scan.nextDouble();
+double SI=(P*R*T)/100;
+System.out.println("Simple interest is"+SI);
+}
+}  */
+
+
+/*
+import java.util.Scanner;
+class Testj{
+public static void main(String ar[]){
+Scanner scan=new Scanner(System.in);
+System.out.println("Enter the principal amount");
+double P=scan.nextDouble();
+System.out.println("Enter the rate of interest");
+double R=scan.nextDouble();
+System.out.println("Enter the number of times interest applied");
+double R=scan.nextDouble();
+System.out.println("Enter the time period");
+double T=scan.nextDouble();
+double A=P(1+R/N)**(N*T);
+System.out.println("The amount to be paid is"+A);
+}
+}
+*/ 
+
+
+/*import java.util.Scanner;
+class Testj{
+public static void main(String ar[]){
+Scanner scan=new Scanner(System.in);
+System.out.println("Enter the amount in dollar");
+double D=scan.nextDouble();
+double R=D*82.68;
+System.out.println("Conversion from dollar to rupee is: "+R);
+}
+}*/
+
+
+//1/2
+/*import java.util.Scanner;
+class Testj{
+public static void main(String ar[]){
+Scanner scan=new Scanner(System.in);
+System.out.println("Enter the base of the triangle");
+double B=scan.nextDouble();
+System.out.println("Enter the height of the triangle");
+double H=scan.nextDouble();
+double P=0.5*(B*H);
+System.out.println("The area of triangle is "+P);
+}
+}*/
+
+
+/*
+import java.util.Scanner;
+class Testj{
+public static void main(String ar[]){
+Scanner scan=new Scanner(System.in);
+System.out.println("Enter the side of square");
+double S=scan.nextDouble();
+double A=4*S;
+System.out.println("The perimeter of square is: "+A);
+}
+}
+*/
+
+
+/*
+import java.util.Scanner;
+
+class circle_area{
+    void find_area(){
+       System.out.println("Area of circle");
+    }
+}
+class Temp_converter{
+    void fa_cel(){
+        System.out.println("fa_cel");
+
+    }
+}
+class Testj{
+    public static void main(String ar[]){
+
+    circle_area objArea=new
+    objArea.find_area();
+	circle_area();
+	}
+        }     */
+	
+/*
+import java.util.Scanner;
+class myCode{
+	int i;
+	void show(){
+	
+		if(i==2){
+System.out.print("2.Line");
+		}
+		else if(i==3){
+System.out.print("3.Line");
+        }
+		else if(i==4){
+System.out.print("4.Line");
+		}else{
+System.out.print("No.Line");
+        }
+	}
+}
+class Testj{
+	int i;
+	public static void main(String ar[]){
+	Scanner scan=new Scanner(System.in);
+	System.out.println("Enter a number from 1 to 4");
+	myCode objMyCode=new myCode();
+	objMyCode.show();
+	}
+}
+*/
+			
+
+
+
+/*
+import java.util.Scanner;
+class myCode{
+	
+	void show(int i){
+	
+		if(i==2){
+System.out.print("2.Line");
+		}
+		else if(i==3){
+System.out.print("3.Line");
+        }
+		else if(i==4){
+System.out.print("4.Line");
+		}else{
+System.out.print("No.Line");
+        }
+	}
+}
+class Test{
+
+	public static void main(String ar[]){
+	int i;
+	Scanner scan=new Scanner(System.in);
+	System.out.println("Enter a number from 1 to 4");
+	i=scan.nextInt();
+	myCode objMyCode=new myCode();
+	objMyCode.show(i);
+	}
+}
+*/
+/*
+class Testj{
+	public static void main(String ar[]){
+     int i=0;
+	 System.out.println(++i-i++);
+	 System.out.println(i);
+	}
+}*/
+
+/*class Test{
+	public static void main(String ar[]);
+	int num1, num2;
+	num1=10;
+	num2=20;
+	String res;
+	 if (num1>num2){
+		 System.out.println("Num1 is bigger")*/
+
+
+
+/*import java.util.Scanner;
+class Testj{
+	public static void main (String ar[]){
+		//int a[]={5,9,1,10};
+    int a;
+	Scanner scan=new Scanner(System.in);
+	System.out.println("Enter a number to check even or odd");
+	a=scan.nextInt();
+	if (a%2==0)
+	System.out.println("The number is even");
+	else
+	System.out.println("The number is odd");
+	
+	}
+}*/
+
+
+
+/*class Testj{
+	public static void main(String ar[]){
+		int a,b;
+		 int temp=1;
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter a number to find its factorial");
+		a=scan.nextInt();
+		for (int i=1;i<=a;i++)
+		//System.out.println(i);
+	    temp=temp*i;
+		System.out.println(temp);
+	}
+}
+*/
+
+//23 FEB
+/*
+class Testj{
+	public static void main(String ar[]){
+		int a,b;
+	   Scanner scan=new Scanner(System.in);
+	   System.out.println("Enter 2 numbers to swap");
+	   a=scan.nextInt();
+	   b=scan.nextInt();
+	   System.out.println("After the numbers are swaped");
+	   a=a+b;
+	   b=a-b;
+	   a=a-b;
+	   System.out.println("a="+a);
+	   System.out.println("b="+b);
+	}
+}  */
+
+
+/*
+class Testj{
+	public static void main(String ar[]){
+		int a,b,c;
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter 2 number to swap using third variable");
+		a=scan.nextInt();
+	    b=scan.nextInt();
+	   System.out.println("After the numbers are swaped");
+	    a=c;
+	    a=b;
+	    b=c;
+	   System.out.println("a="+a);
+	   System.out.println("b="+b);
+	}
+} */
+
+
+
+/* 
+class Testj{
+   public static void main(String ar[]){
+    float num1,num2;
+    Scanner scan=new Scanner(System.in);
+    System.out.println("Enter number 1");
+	num1=scan.nextInt();
+	System.out.println("Enter number 2");
+	num2=scan.nextInt();
+	System.out.println("The answer is: ");
+	if ((num1==0)||(num2==0))
+	{
+		System.out.println("Error:Do not enter 0 as a value");
+	}
+	else if(num1>num2){
+	   System.out.println(num1/num2);
+	}
+	else{
+	   System.out.println(num2/num1);
+    }
+		
+   }
+}   
+*/
+
+
+/*
+class Testj{
+	public static void main(String ar[]){
+		double x,n,exponent;
+		Scanner scan=new Scanner(System.in);
+	System.out.println("This is a program to find x to the power n");
+	System.out.println("Enter the value for x");
+	  x=scan.nextInt();
+	System.out.println("Enter the value for n");
+	  n=scan.nextInt();
+	exponent=Math.pow(x,n);
+	System.out.println("The value is: "+exponent);
+	}
+ }*/
+	
+
+
+
+
+/*
+import java.util.Scanner;
+class Testj{
+	public static void main(String ar[]){
+	   double A,B,add,sub,div,mul ;
+	   Scanner scan=new Scanner(System.in);
+	System.out.println("The program is to find the sum,sub,div and mul of 2 numbers");
+    System.out.println("Enter the value of A: ");
+	      A=scan.nextDouble();
+	System.out.println("Enter the value of B: ");
+	      B=scan.nextDouble();
+	  add=A+B;
+	  sub=A-B;
+	  mul=A*B;
+	  div=A/B;
+	System.out.println("The expected outcome is: ");
+	System.out.println("addition: "+add);
+	System.out.println("substraction:"+sub);
+	System.out.println("multiplication:"+mul);
+	System.out.println("division:"+div);
+	}
+	
+} */  
+/*
+import java.util.Scanner;
+class Testj{
+	public static void main(String ar[]){
+		Scanner scan=new Scanner(System.in);
+		   int num1;
+		System.out.println("Enter the number for the multiplication table: ");
+		   num1=scan.nextInt();
+		
+		for(int i=0; i<10; i++){
+			System.out.println(num1 + "X" + (i+1) + "=" +num1*(i+1));
+		}	
+	}
+}
+*/
+/* 
+import java.util.Scanner;
+class Testj{
+	public static void main (String ar[]){
+	Scanner scan=new Scanner (System.in);
+	
+	System.out.println("    j      A     V     V     A     ");
+	System.out.println("    j     A A     V   V     A A    ");
+    System.out.println("j   j    A A A     V V     A A A   ");
+	System.out.println(" j j    A     A     V     A     A  ");
+	}
+}
+*/
+
+
+/*
+import java.util.Scanner;
+ class Testj{
+ public static void main (String ar[]){
+	 int a,b;
+   Scanner scan=new Scanner(System.in);
+   System.out.println("Enter first number");
+   a=scan.nextInt();
+   System.out.println("Enter second number");
+   b=scan.nextInt();
+   
+  if(a==b)
+   System.out.println("a==b");
+  if(a!=b)
+   System.out.println("a!=b");
+  if(a>b)
+   System.out.println("a>b");
+  if(a<b)
+   System.out.println("a<b");
+  if(a>=b)
+   System.out.println("a>=b");	  
+  if(a<=b)
+   System.out.println("a<=b");	  
+  }
+} 
+*/   
+
+// Sum of digits of a number
+
+/*
+import java.util.Scanner;
+
+class Testj {
+    public static void main(String[] args) {
+        long num, sum = 0;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number to find sum of digits: ");
+        num = scan.nextLong();
+        sum = sum(num);
+        System.out.println(sum);
+    }
+
+    public static long sum(long num) {
+        long sum = 0;
+        while (num != 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
+    }
+}
+*/
+
+ 
+/*
+//Area of hexagon
+import java.util.Scanner;
+ class Testj{
+	 public static void main (String ar[]){
+		 double side;
+		 double area;
+		 Scanner scan= new Scanner(System.in);
+		 System.out.println("Enter the side of hexagon to find the area: ");
+		 side=scan.nextInt();
+		 area=((6*Math.pow(side,2))/(4*Math.tan(3.14/6)));
+		 System.out.println(area);
+	}
+ }
+*/	
+	 
+/*
+//Area of polygon
+import java.util.Scanner;
+ class Testj{
+	 public static void main (String ar[]){
+		 double no_side,len_side,area;
+		 Scanner scan =new Scanner(System.in);
+		 System.out.println("Enter the number of sides in the polygon: ");
+		 no_side=scan.nextDouble();
+		 System.out.println("Enter the length of the sides: ");
+		 len_side=scan.nextDouble();
+		 area=(no_side*Math.pow(len_side,2))/(4*Math.tan(3.14/no_side));
+		 System.out.println(area);
+	}
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
